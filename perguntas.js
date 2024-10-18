ac1dc0941a90ebe608165aee04324fba22656a27
AnaPicller01 <164193234+AnaPicller01@users.noreply.github.com>
2024-10-18 12:26:29 -0300

    18/10

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+     'Categoria',
+ 'Qual é a ordem dos produtos para o pré-make?',
+ 'Primeiro você lava o seu rosto para tirar toda a sujidade, passa um tônico que retira as impureas profundas, hidratante do seu tipo de pele e para '
+)
+
+criaCartao(
+    'Categoria',
+'Pergunta?',
+'Respostas'
+)
+
+criaCartao(
+    'Categoria',
+'Pergunta?',
+'Respostas'
+)
+
+criaCartao(
+    'Categoria',
+'Pergunta?',
+'Respostas'
+)
