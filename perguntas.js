a3cdb5977949e6d52615553fe736a87ee8aad27b
AnaPicller01 <164193234+AnaPicller01@users.noreply.github.com>
2024-10-25 08:39:47 -0300

    25/10

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-     'Categoria',
+     'Pré-make',
  'Qual é a ordem dos produtos para o pré-make?',
- 'Primeiro você lava o seu rosto para tirar toda a sujidade, passa um tônico que retira as impureas profundas, hidratante do seu tipo de pele e para '
+ 'Primeiro você lava o seu rosto para tirar toda a sujidade, passa um tônico que retira as impureas profundas, hidratante do seu tipo de pele e para ter uma maquiagem mais resistente é importante o uso da bildagem. '
 )
 
 criaCartao(
-    'Categoria',
-'Pergunta?',
-'Respostas'
+    'Subtom de pele',
+'Como identificar o seu subtom de pele?',
+'Tem que ter uma base do seu tom, identificar se essa base está alaranjada, escura ou rosada. Em casos de laranja você adiciona um pouco de pigmento azul, se estiver escura o pigmento amarelado com um pouquinho de branco e rosada você adiciona verde.'
 )
 
 criaCartao(
-    'Categoria',
-'Pergunta?',
-'Respostas'
+    'Cílios postiços',
+'Cmo aplicar o cílios postiços corretamente?',
+'Corte um cílios ao meio, você terá um par, passe a cola para cílios e deixe secar até ver que ela está azulada, após alique no canto externo do seu olho, pegue uma pinça ou com os dedos mesmo junta os cílios naturais com os sintéticos.'
 )
 
 criaCartao(
-    'Categoria',
-'Pergunta?',
-'Respostas'
+    'Maquiadora da Virginia',
+'Qual é o nome da maquiadora da Virginia?',
+'Priless'
 )
